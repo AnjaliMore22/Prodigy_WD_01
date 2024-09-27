bb0811dab28f5f0cf20ea6a0dbe97be5eb8dce48
Anjali More <anjalidjmore@gmail.com>
2024-09-28 01:28:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', function() {
         const navbar = document.getElementById('navbar');
+        const title = document.getElementById('title');
+
+        // Change navbar color on scroll
         if (window.scrollY > 50) {
             navbar.classList.add('scrolled');
         } else {
